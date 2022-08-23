9ef3aaa76fa5557d2f2f6c76683a147f46584039
Anshumaan Singh <anshumaansingh255@gmail.com>
2022-08-23 22:33:06 +0530

    adding props

--- reactfirst/contact/src/components/contactList.js
+++ reactfirst/contact/src/components/contactList.js
@@ -1,6 +1,19 @@
 import React from 'react';
 
-const ContactList = () => {
+const ContactList = (props) => {
+    console.log(props);
+
+    const renderContactList = props.contacts.map((contact)=>{
+        return (
+            <div className="item">
+                <div className="contact">
+                    <div className="header">{contact.name}
+
+                    </div>
+                </div>
+            </div>
+        )
+    })
     return(
         <div className="ui celled list">
             Contact List
