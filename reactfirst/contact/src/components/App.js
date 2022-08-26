d0bea19ba4efb69d3875bd548086130491074cac
Anshumaan Singh <anshumaansingh255@gmail.com>
2022-08-26 12:56:07 +0530

    adding hooks in app.js

--- reactfirst/contact/src/components/App.js
+++ reactfirst/contact/src/components/App.js
@@ -1,22 +1,11 @@
 import './App.css';
-import React from "react";
+import React, {userState} from "react";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./contactList"; 
 
 function App() {
-  const contacts=[
-    {
-      id: "1",
-      "name": "Sarah",
-      "email": "sarah545@gamil.com",
-    },
-    {
-      id: "2",
-      "name": "Anshumaan",
-      "email": "anshu@gmail.com",
-    },
-  ]
+   const [contacts,setContacts] = useState([]);
   return (
     <div className="ui container">
       <Header />
