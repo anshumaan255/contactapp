b5b18a86e8fc5547be1760867b18ea7f9f8ca349
bruteF04C3 <rishabhonweb@gmail.com>
2022-09-04 12:39:03 +0530

    Update contact UI

--- reactfirst/contact/src/components/contactCard.js
+++ reactfirst/contact/src/components/contactCard.js
@@ -1,20 +1,43 @@
-import React from 'react';
-import user from "../images/user.png"
-const ContactCard = (props ) => {
-    const {id , name,email}=props.contact;
-    return(    
-     <div className="item">
-     <img className="ui avatar image"src ={user} alt ="user" />
-
-    <div className="contact">
-        <div className="header">{name}
-        <div>{email}</div>
+import React from "react";
+import user from "../images/user.png";
+const ContactCard = (props) => {
+  const { id, name, email } = props.contact;
+  return (
+    <>
+      <div
+        className="contact-item"
+        style={{ width: "100%", height: "fit-content", display: "flex", justifyContent: 'flex-start', alignItems: 'center', position: 'relative', margin: '20px 0' }}
+      >
+        <div>
+          <img className="ui avatar image" src={user} alt="user" />
         </div>
-    </div>
-    <i className="trash alternate outline icon"
-    style={{color: "red", marginTop:"7px"}}></i>
-</div>
- );
+        <div>
+          <div className="contact" style={{ marginLeft: '20px' }}>
+            <p style={{ fontWeight: 'bold', margin: '0' }}>{name}</p>
+            <p style={{ fontWeight: 'thin', margin: '0' }}>{email}</p>
+          </div>
+        </div>
+        <div style={{ position: 'absolute', right: '0' }}>
+          <i
+            className="trash alternate outline icon"
+            style={{ color: "red", fontSize: '20px' }}
+          ></i>
+        </div>
+      </div>
+      <hr />
+    </>
+    //      <div className="item">
+    //      <img className="ui avatar image"src ={user} alt ="user" />
+
+    //     <div className="contact">
+    //         <div className="header">{name}
+    //         <div>{email}</div>
+    //         </div>
+    //     </div>
+    //     <i className="trash alternate outline icon"
+    //     style={{color: "red", marginTop:"7px"}}></i>
+    // </div>
+  );
 };
 
 export default ContactCard;
