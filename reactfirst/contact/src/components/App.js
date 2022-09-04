ab0b572a1488c58e08c2f327cfaa3ea8471fead9
Anshumaan Singh <anshumaansingh255@gmail.com>
2022-09-04 12:07:11 +0530

    useeffect react hook

--- reactfirst/contact/src/components/App.js
+++ reactfirst/contact/src/components/App.js
@@ -1,15 +1,29 @@
 import './App.css';
-import React, {userState} from "react";
+import React, {useState, useEffect, useInsertionEffect} from "react";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./contactList"; 
 
 function App() {
+   const LOCAL_STORAGE_KEY = "contacts";
    const [contacts,setContacts] = useState([]);
+
+   const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts,contact]);
+   };
+   useEffect(() => {
+     const retriveContacts = JSON.parse( localStorage.getItem(LOCAL_STORAGE_KEY ));
+     if(retriveContacts)setContacts(retriveContacts);
+   }, []);
+
+   useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY ,JSON.stringify(contacts));
+   }, [contacts]);
   return (
     <div className="ui container">
       <Header />
-      <AddContact />
+      <AddContact addContactHandler={addContactHandler} />
       <ContactList contacts ={contacts} />
     </div>
   );
